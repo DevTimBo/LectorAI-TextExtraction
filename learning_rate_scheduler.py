30843d82cb322b3e2147861c49d36e982846de9a
Jason Pranata <jasonpranata01@gmail.com>
2024-05-09 00:42:35 +0200

    lr scheduler works

--- learning_rate_scheduler.py
+++ learning_rate_scheduler.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 class lr_scheduler:
 
@@ -10,41 +11,41 @@ class lr_scheduler:
         self.warmup_steps = warmup_steps
         self.name = name
         
-    def get_scheduler(self):
+    def __call__(self):
         if self.name == "cosine_decay":
             return tf.keras.optimizers.schedules.CosineDecay(
                 initial_learning_rate=self.initial_learning_rate,
                 decay_steps=self.decay_steps,
                 alpha=self.alpha,
-                warmup_target=self.warmup_target,
-                warmup_steps=self.warmup_steps,
-                name="cosine_decay",
+                warmup_target = self.warmup_target,
+                warmup_steps = self.warmup_steps
             )
         elif self.name == "cosine_decay_restarts":
             return tf.keras.optimizers.schedules.CosineDecayRestarts(
                 initial_learning_rate=self.initial_learning_rate,
                 first_decay_steps=self.decay_steps,
                 alpha=self.alpha,
-                warmup_target=self.warmup_target,
-                warmup_steps=self.warmup_steps,
-                name="cosine_decay_restarts",
+                warmup_target = self.warmup_target,
+                warmup_steps = self.warmup_steps
             )
         elif self.name == "exponential_decay":
             return tf.keras.optimizers.schedules.ExponentialDecay(
                 initial_learning_rate=self.initial_learning_rate,
                 decay_steps=self.decay_steps,
                 decay_rate=self.alpha,
-                staircase=True,
-                name="exponential_decay"
+                staircase=True
                 )
         elif self.name == "inverse_time_decay":
             return tf.keras.optimizers.schedules.InverseTimeDecay(
                 initial_learning_rate=self.initial_learning_rate,
                 decay_steps=self.decay_steps,
                 decay_rate=self.alpha,
-                staircase=True,
-                name="inverse_time_decay"
+                staircase=True
                 )
-        
-    def __call__(self, step):
-        
+        elif self.name == "polynomial_decay":
+            return tf.keras.optimizers.schedules.PolynomialDecay(
+                initial_learning_rate=self.initial_learning_rate,
+                decay_steps=self.decay_steps,
+                end_learning_rate=self.warmup_target,
+                power=1.0
+                )
