8a64e91ff2a9c9cad8beb56976a6a776c675a941
Jason Pranata <jasonpranata01@gmail.com>
2024-05-17 20:33:27 +0200

    update bbox inference with conversion (untested but old one still implemented in main)

--- inference_bbox.py
+++ inference_bbox.py
@@ -1,6 +1,11 @@
 import cv2
 import keras_cv
 import numpy as np
+import onnx
+import onnx_tf
+import tensorflow as tf
+import ultralytics
+
 
 
 CLASSES =[
@@ -59,10 +64,38 @@ class bbox_model:
         resized_image = cv2.resize(image, (1536, 768))
         resized_image = np.expand_dims(resized_image, axis=0)
         predictions = self.model.predict(resized_image)
+        return self._extracted_predictions(predictions)
+    
+    def converted_model_inference(self, model_weight_path: str, image_path):
+        model = self.convert_torch_to_tf(model_weight_path)
+        # Preprocessing
+        image = cv2.imread(image_path) 
+
+        # Placeholder
+        predictions = model.predict(image)
+
+        return self._extracted_predictions(predictions)
+
+    def _extracted_predictions(self, predictions):
         boxes = predictions['boxes'][0]
         confidence = predictions['confidence'][0]
         classes = predictions['classes'][0]
         return boxes, confidence, classes
+    
+    def convert_torch_to_tf(self, weights_path):
+        # Load YOLOv8 model
+        model = ultralytics.YOLO(weights_path)  # Replace with your model path
+
+        # Convert to ONNX
+        success = model.export(format='onnx')
+        if not success:
+            raise RuntimeError("ONNX export failed")
+        onnx_model = onnx.load(f'{weights_path}.onnx')
+        tf_rep = onnx_tf.backend.prepare(onnx_model)
+        tf_rep.export_graph(f"{weights_path}/tf_model")
+        
+        return tf.saved_model.load(f"{weights_path}/tf_model")
+
 
 bbox_model = bbox_model()
 
