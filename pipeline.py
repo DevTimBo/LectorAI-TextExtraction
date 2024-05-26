e39b9a45f9f13acf055fdc39a38f64603ba3520b
Jason Pranata <jasonpranata01@gmail.com>
2024-05-26 13:04:03 +0200

    logging in pipeline-- save image with bounding boxes(new), save cropped images(already there), added print statements as feedback

--- pipeline.py
+++ pipeline.py
@@ -49,8 +49,18 @@ class pipeline:
     def __call__(self, directory, filename):
         image = tf.io.read_file(os.path.join(directory, filename))
         image = tf.image.decode_png(image, channels=3)
+        print("Image loaded successfully.")
         results = self._predict_bounding_boxes(image)
+        print("Bounding boxes predicted successfully.")
         classes, boxes, confidences = zip(*results)
+
+        full_img_np = image.numpy()
+        full_img_np = cv2.cvtColor(full_img_np, cv2.COLOR_RGB2BGR)
+        for label, box, score in results:
+            x_min, y_min, x_max, y_max = box
+            cv2.rectangle(full_img_np, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+            cv2.putText(full_img_np, f'{label}: {score:.2f}', (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
+        cv2.imwrite(f"tempimages_api/document_with_bounding_boxes.png", full_img_np)
 
         cropped_images = []
         image_np = image.numpy()
@@ -66,7 +76,8 @@ class pipeline:
             crop_bottom_percent = float(params["bottom"])
             cropped_image = crop(x_min, y_min, x_max, y_max, image_np, crop_left_percent, crop_bottom_percent)
             cropped_images.append(cropped_image)
-
+        print("Cropped images successfully.")
+        
         text_predictions = []
         for i, img in enumerate(cropped_images):
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -74,12 +85,16 @@ class pipeline:
             prediction = handwriting_model.inference(gray_with_dim)
             text_predictions.append(prediction)
             cv2.imwrite(f"tempimages_api/cropped_{i}.png", img)
-
+            print("Text predicted successfully")
+        
         predictions = []
         for i, prediction in enumerate(text_predictions):
             x1, y1, x2, y2 = boxes[i]
             predictions.append({"class": classes[i], "prediction": prediction, "confidence": float(confidences[i]), "box": [x1, y1, x2, y2]})
-        return jsonify({"predictions":predictions}) # if testing here change to print instead
+        print("Returning JSON response.")
+        # if testing with this script change to print instead
+        # print(predictions)
+        return jsonify({"predictions":predictions}) 
 
 pipeline = pipeline()
 pipeline("tempimages_api", "beispiel_form.jpg")
